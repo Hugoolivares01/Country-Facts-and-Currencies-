a1feac7defe1c29c2edd6a047b19c069ed38a50b
tbohn2 <tanner.bohn@yahoo.com>
2023-03-30 19:47:00 -0700

    API URLs placed in JS

--- assets/select.js
+++ assets/select.js
@@ -1,8 +1,17 @@
-fetch('https://restcountries.com/v3.1/name/mexico', {
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+// fetch('https://restcountries.com/v3.1/name/mexico', {
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+
+// fetch('https://api.frankfurter.app/latest?amount=10&from=GBP&to=USD', {
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
