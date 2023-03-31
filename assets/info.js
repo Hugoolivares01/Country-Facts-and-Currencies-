c01e597a84f3419662e55f8df87ab2adf0825149
tbohn2 <tanner.bohn@yahoo.com>
2023-03-30 22:01:13 -0700

    Mock country info page w/ styles

---
+++ assets/info.js
@@ -0,0 +1,26 @@
+// fetch('https://restcountries.com/v3.1/all', {
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+
+// fetch('https://restcountries.com/v3.1/name/italy', {
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+
+// fetch('https://api.frankfurter.app/latest?amount=10&from=GBP&to=USD', {
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
