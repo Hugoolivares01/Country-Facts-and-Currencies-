90909af8a838691ef8aa6783520d6782d4ab227b
Juice <hugoolivares01@gmail.com>
2023-04-02 20:02:14 -0700

    place holder code in info js for pullin search value

--- assets/info.js
+++ assets/info.js
@@ -24,11 +24,30 @@
 //     .then(function (data) {
 //         console.log(data);
 //     });
-fetch('https://api.frankfurter.app/latest?amount=10&from=GBP&to=USD', {
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+// fetch('https://api.frankfurter.app/latest?amount=10&from=GBP&to=USD', {
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+
+// function getParams() {
+//     // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+//     var searchParamsArr = document.location.search.split('&');
+  
+//     // Get the query and format values
+//     var query = searchParamsArr[0].split('=').pop();
+//     var format = searchParamsArr[1].split('=').pop();
+  
+//     searchApi(query, format);
+//   }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var SearchLine = document.location.search;
+    var SearchSplit = SearchLine.split('=');
+    var SearchValue = SearchSplit[1];
+    // var query = searchParamsArr[0].split('=').pop();
+    console.log(SearchValue);
+});
